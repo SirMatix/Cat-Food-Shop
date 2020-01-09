9b79d5d25727f62921a596a9667431bf241cbb6a
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-01-09 18:55:06 +0000

    Add total after entering number of elements and press any key to exit msg

--- CatFoodShop/CatFoodShop/Program.cs
+++ CatFoodShop/CatFoodShop/Program.cs
@@ -20,6 +20,10 @@ namespace CatFoodShop
             Console.WriteLine("Welcome to the cat food store!");
             string entry = Ask("How many cans are you ordering?");
             Console.Write($"You entered {entry} cans");
+            int number = int.Parse(entry);
+            Console.WriteLine($"For {entry} cans, yout total is: ${number * 2}");
+            Console.Write("Press any key to close...");
+            Console.ReadLine();
             
         }
     }
