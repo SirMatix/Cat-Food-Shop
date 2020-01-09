0fec5f2992e82ad3f6b77284a383870aa0db3311
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-01-09 11:19:53 +0000

    Added ask method and welcome message

---
+++ CatFoodShop/CatFoodShop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatFoodShop
+{
+    class Program
+    {
+        static string Ask(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the cat food store!");
+            string entry = Ask("How many cans are you ordering?");
+            Console.Write(entry);
+        }
+    }
+}
